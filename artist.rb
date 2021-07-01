ccbbc3623b3e5236788bd211594673ca21626dd0
Lj <tnedutsl457892@gmail.com>
2021-07-01 07:04:01 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -3,14 +3,5 @@ class Artist < ActiveRecord::Base
   def self.create_table
     # create table using SQL here
   end
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY,
-      title TEXT,
-      length INTEGER
-    )
-    SQL
 
-    ActiveRecord::Base.connection.execute(sql)
-  end
 end
